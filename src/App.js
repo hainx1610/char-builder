35785b27955c996968a26a7e316a3411c51ca0b2
Harris Nguyen <hainx1610@gmail.com>
2023-02-10 08:49:19 +0700

    add a static UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+function Avatar() {
+  return <div>ava</div>;
+}
+
+function Square() {
+  // const testImgLink =
+  //   "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1200px-Square_-_black_simple.svg.png";
+  return <img src={require("./character/body/1.png")} alt="" width={"40px"} />;
+}
+function ItemsList() {
+  return (
+    <div className="flex">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
+
+function PartListSection() {
+  return (
+    <div>
+      <h2>Name of list</h2>
+      <ItemsList />
+    </div>
+  );
+}
+function PartListsArea() {
+  return (
+    <div>
+      <PartListSection />
+      <PartListSection />
+      <PartListSection />
+    </div>
+  );
+}
+
+function MainArea() {
+  return (
+    <div>
+      <Avatar />
+      <PartListsArea />
+    </div>
+  );
+}
 
 function App() {
-  return <div>Let's make an app</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <div className="title text-3xl text-blue-600">CHARACTER</div>
+      <div className="title text-3xl text-blue-600">customization</div>
+      <MainArea />
+    </div>
+  );
 }
 
 export default App;
