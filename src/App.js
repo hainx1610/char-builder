d5efeb1435128c82a333fe92a3927af339b2b829
Harris Nguyen <hainx1610@gmail.com>
2023-02-11 18:13:06 +0700

    add click event to handle rendering avatar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const total = {
   body: 17,
@@ -15,24 +15,76 @@ const total = {
   layer_1: 5,
   layer_2: 5,
   layer_3: 9,
+  noses: 1,
 };
 
-function Avatar() {
-  return <div>ava</div>;
+function Avatar({ avatarObj }) {
+  return (
+    <div className="relative h-[280px] w-[260px]">
+      {Object.entries(avatarObj).map((item, index) => {
+        const [listName, number] = item;
+        let subfolder = listName;
+        let subIndex = number;
+        if (
+          listName === "earrings" ||
+          listName === "glasses" ||
+          listName === "hats" ||
+          listName === "neckwear"
+        ) {
+          subfolder = `accessories/${listName}`;
+        }
+
+        if (
+          listName === "layer_1" ||
+          listName === "layer_2" ||
+          listName === "layer_3"
+        ) {
+          subfolder = `clothes/${listName}`;
+        }
+        return (
+          <img
+            src={require(`./character/${subfolder}/${subIndex}.png`)}
+            alt=""
+            width={260}
+            className={`absolute top-0 left-0 z-[${index}]`}
+            key={index}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
-function Square({ subfolder, subIndex }) {
+function RandomizeBtn() {
+  return (
+    <button className=" bg-blue-600 text-white hover:cursor-pointer hover:brightness-90">
+      Randomize
+    </button>
+  );
+}
+
+function AvatarWrapper({ avatarObj }) {
+  return (
+    <div className="flex flex-col items-center">
+      <Avatar avatarObj={avatarObj} />
+      <RandomizeBtn />
+    </div>
+  );
+}
+
+function Square({ subfolder, subIndex, handleClick }) {
   return (
     <img
       src={require(`./character/${subfolder}/${subIndex}.png`)}
       alt=""
       width={"40px"}
-      className=" rounded-sm border bg-slate-100 hover:cursor-pointer hover:bg-slate-50"
+      className=" rounded-sm border bg-blue-200 hover:cursor-pointer hover:brightness-90"
+      onClick={() => handleClick(subfolder, subIndex)}
     />
   );
 }
 
-function ItemsList({ listName, number }) {
+function ItemsList({ listName, number, handleClick }) {
   let tempArr = [];
   for (let i = 1; i <= number; i++) {
     let subfolder = listName;
@@ -53,28 +105,44 @@ function ItemsList({ listName, number }) {
       subfolder = `clothes/${listName}`;
     }
 
-    tempArr.push(<Square subfolder={subfolder} subIndex={i} key={i} />);
+    tempArr.push(
+      <Square
+        subfolder={subfolder}
+        subIndex={i}
+        key={i}
+        handleClick={handleClick}
+      />
+    );
   }
   return <div className="flex flex-wrap justify-center">{tempArr}</div>;
 }
 
-function PartListSection({ listName, number }) {
+function PartListSection({ listName, number, handleClick }) {
   return (
     <div className="my-8 flex flex-col items-center">
       <h2>{listName}</h2>
-      <ItemsList listName={listName} number={number} />
+      <ItemsList
+        listName={listName}
+        number={number}
+        handleClick={handleClick}
+      />
     </div>
   );
 }
 
-function PartListsArea() {
+function PartListsArea({ handleClick }) {
   return (
     <div>
       {Object.entries(total).map((item, index) => {
         const [listName, number] = item;
 
         return (
-          <PartListSection listName={listName} number={number} key={index} />
+          <PartListSection
+            listName={listName}
+            number={number}
+            key={index}
+            handleClick={handleClick}
+          />
         );
       })}
     </div>
@@ -82,10 +150,32 @@ function PartListsArea() {
 }
 
 function MainArea() {
+  const [avatarObj, setAvatarObj] = useState({
+    body: 1,
+    eyes: 1,
+    hair: 1,
+    mouths: 1,
+    eyebrows: 1,
+
+    earrings: 1,
+    glasses: 1,
+    hats: 1,
+    neckwear: 1,
+    facial_hair: 1,
+    layer_1: 1,
+    layer_2: 1,
+    layer_3: 1,
+    noses: 1,
+  });
+
+  const handleClick = (subfolder, subIndex) => {
+    setAvatarObj({ ...avatarObj, [subfolder]: [subIndex] });
+  };
+
   return (
     <div>
-      <Avatar />
-      <PartListsArea />
+      <AvatarWrapper avatarObj={avatarObj} />
+      <PartListsArea handleClick={handleClick} />
     </div>
   );
 }
